ecc549d69aff1616e1d149466134e8d8c4302313
3oheme <bohemegm@gmail.com>
2012-11-23 11:13:17 +0000

    @3oheme First commit - only dirty stuff for spikes

---
+++ add_backround.js
@@ -0,0 +1,14 @@
+ var p_line_height = parseInt($('p:last').css('line-height'));
+
+  $('<canvas style="display: none" width="10px" height="100px" id="bgcanvas" ></canvas>').appendTo('body');
+
+  var canvas = document.getElementById("bgcanvas");
+
+  canvas.width = 1;
+  canvas.height = p_line_height;
+
+  var context = canvas.getContext("2d");
+  context.fillStyle = "rgba(0, 0, 0, .1)";
+  context.fillRect(0, 0, p_line_height, 1);
+
+  $('body').css('background', 'url("'+ canvas.toDataURL() +'")');
