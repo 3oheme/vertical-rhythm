b8b59a450575b5b37a83885ffd92dbf5a0a716b8
3oheme <bohemegm@gmail.com>
2012-11-23 17:52:41 +0000

    @3oheme nearly there!

--- popup.js
+++ popup.js
@@ -2,36 +2,25 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+
 function click(e) {
+  var p_line_height = document.getElementById('spacing').value;
+
+  var canvas = document.getElementById("bgcanvas");
+  
+  canvas.width = 1;                                                             
+  canvas.height = p_line_height;                                                
+                                                                                
+  var context = canvas.getContext("2d");                                        
+  context.fillStyle = "rgba(0, 0, 0, .1)";                                      
+  context.fillRect(0, 0, p_line_height, 1);
+  var image = canvas.toDataURL()
+
   chrome.tabs.executeScript(null,
-      {code:"$('#main').hide()"});
+      {code:"document.body.style.backgroundImage='url("+ canvas.toDataURL()  +")';"});
   window.close();
 }
 
-
-/*
-
-code:"console.log(nt.body.style.backgroundColor='" + e.target.id + "'"
-
-  var p_line_height = parseInt($('p:last').css('line-height'));
-
-  $('<canvas style="display: none" width="10px" height="100px" id="bgcanvas" ></canvas>').appendTo('body');
-
-  var canvas = document.getElementById("bgcanvas");
-
-  canvas.width = 1;
-  canvas.height = p_line_height;
-
-  var context = canvas.getContext("2d");
-  context.fillStyle = "rgba(0, 0, 0, .1)";
-  context.fillRect(0, 0, p_line_height, 1);
-
-  $('body').css('background', 'url("'+ canvas.toDataURL() +'")');
-*/
-
 document.addEventListener('DOMContentLoaded', function () {
-  var divs = document.querySelectorAll('div');
-  for (var i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('click', click);
-  }
+  document.querySelector('#apply').addEventListener('click', click);
 });
