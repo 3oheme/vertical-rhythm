055f5b4bdfec2fefb4079fd8b1544ce76143588d
3oheme <ignacio.palomo.duarte@gmail.com>
2013-10-30 13:09:17 +0100

    @3oheme added offset control to vertical rhythm

--- popup.js
+++ popup.js
@@ -1,29 +1,23 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-
 function click(e) {
   var p_line_height = document.getElementById('spacing').value;
+  var p_offset = document.getElementById('offset').value;
 
   var canvas = document.getElementById("bgcanvas");
   
-  canvas.width = 1;                                                             
-  canvas.height = p_line_height;                                                
+  canvas.width = 1;
+  canvas.height = p_line_height;
                                                                                 
-  var context = canvas.getContext("2d");                                        
-  context.fillStyle = "rgba(0, 0, 0, .1)";                                      
+  var context = canvas.getContext("2d");
+  context.fillStyle = "rgba(0, 0, 0, .1)";
   context.fillRect(0, 0, p_line_height, 1);
   var image = canvas.toDataURL()
 
-
   chrome.tabs.executeScript(null,
-      {code:"document.body.style.backgroundImage='url("+ canvas.toDataURL()  +")';"});
-  //window.close();
-
+      {code:"document.body.style.backgroundImage='url("+ image  +")';document.body.style.backgroundPosition='0 " + p_offset + "px';"});
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+  click();
   document.querySelector('#apply').addEventListener('click', click);
 });
 
