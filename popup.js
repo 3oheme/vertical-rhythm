5a019c4e3e455329428a1b2828944f34624175fe
3oheme <ignacio.palomo.duarte@gmail.com>
2013-10-31 13:42:43 +0100

    @3oheme added storage sync support

--- popup.js
+++ popup.js
@@ -1,7 +1,9 @@
 function click(e) {
+
+  load_url_configuration();
+
   var p_line_height = document.getElementById('spacing').value;
   var p_offset = document.getElementById('offset').value;
-
   var canvas = document.getElementById("bgcanvas");
   
   canvas.width = 1;
@@ -14,17 +16,32 @@ function click(e) {
   context.fillRect(0, p_line_height/2, p_line_height, 1);
   var image = canvas.toDataURL()
 
+  set_url_configuration(p_line_height, p_offset, 'example.com');
+
   chrome.tabs.executeScript(null,
-      {code:"document.body.style.backgroundImage='url("+ image  +")';document.body.style.backgroundPosition='0 " + p_offset + "px';"});
+      {code:"document.body.style.backgroundImage='url("+ image  +")';document.body.style.backgroundPosition='0 " + p_offset + "px';"}); 
+}
+
+function set_url_configuration(spacing, offset, url) {
+  var site_config = {};
+  site_config['spacing'] = spacing;
+  site_config['offset'] = offset;
+  var current_url = url;
+  
+  chrome.storage.sync.set({current_url: site_config});
+}
+
+function load_url_configuration() {
+  chrome.storage.sync.get('example.com', function(site_configuration) {
+    if (typeof site_configuration != 'undefined') {
+      document.getElementById('spacing').value = site_configuration['spacing'];
+      document.getElementById('offset').value = site_configuration['offset'];
+    }
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+  load_url_configuration();
   click();
   document.querySelector('#apply').addEventListener('click', click);
-});
-
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    document.getElementById('spacing').value = 88;
-   // document.getElementById('spacing').value = request.size;
 });
